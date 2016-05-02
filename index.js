6307b6f7f31a5e2d413cee952d6bdbb295ae41f5
Brian Walsh <brian.walsh3@mycit.ie>
2016-05-02 08:40:21 +0100

    another commit

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 var Xray = require('x-ray');
 var x = Xray();
+var i=1;
 
-x('http://www.futhead.com/16/players/random/', 'body', [{
-    player: 'h1 a',
-    position: '.playercard-position',
-    rating: '.playercard-rating',
-    nation: '.playercard-nation img@src',
-    club: '.playercard-club img@src',
-    playerImage: '.playercard-picture img@src',
-    stats: [{
-        pace: '.playercard-attr1',
-        shoting: '.playercard-attr2',
-        passing: '.playercard-attr3',
-        dribbling: '.playercard-attr4',
-        defending: '.playercard-attr5',
-        physical: '.playercard-attr6'
-    }],
-    workrates: '.playercard-workrates'
+	x('http://www.futhead.com/16/players/1/', 'body', [{
+		player: 'h1 a',
+		position: '.playercard-position',
+		rating: '.playercard-rating',
+		nation: '.playercard-nation img@src',
+		club: '.playercard-club img@src',
+		playerImage: '.playercard-picture img@src',
+		workrates: '.playercard-workrates',
+		stats: x('.card-small', {
+		  pace: '.playercard-attr1',
+		  shooting: '.playercard-attr2',
+		  passing: '.playercard-attr3',
+		  dribbling: '.playercard-attr4',
+		  defending: '.playercard-attr5',
+		  physical: '.playercard-attr6'
+			})
+		
+}])(function(err, title) {
+  console.log(title) // Google 
+}).write('results.json')
 
-}]).limit(1)
-    .write('results.json')
+console.log("json file created")
+
