86231398901148777aab01cf3ef3ddff3342f422
Brian Walsh <brian.walsh3@mycit.ie>
2016-05-04 12:00:49 +0100

    Final commit, probably

--- README.md
+++ README.md
@@ -7,5 +7,11 @@ $ npm install brianwalsh-bw
 Calling the package
 ```sh
 var assignment = require('assignment-bw');
+
+var playerRating = 1; //gets the number 1 rated player
+var club = 'arsenal'; //gets you players from this club
+
+assignment.scrapePlayers(playerRating)
+assignment.scrapeClubs(club);
 ```
 
--- index.js
+++ index.js
@@ -1,27 +1,62 @@
 var Xray = require('x-ray');
-var x = Xray();
-var i = 4;
 
-	x('http://www.futhead.com/16/players/' + i, 'body', [{
-		player: 'h1 a',
-		position: '.playercard-position',
-		rating: '.playercard-rating',
-		nation: '.playercard-nation img@src',
-		club: '.playercard-club img@src',
-		playerImage: '.playercard-picture img@src',
-		workrates: '.playercard-workrates',
-		stats: x('.card-large', {
-		  pace: '.playercard-attr1',
-		  shooting: '.playercard-attr2',
-		  passing: '.playercard-attr3',
-		  dribbling: '.playercard-attr4',
-		  defending: '.playercard-attr5',
-		  physical: '.playercard-attr6'
-			})
-		
-}])(function(err, title) {
-  console.log(title);  
-  console.log("json file created");
-}).write('results.json')
+var futPlayer = function scrapePlayers(playerRating){
+    var x = Xray();
 
+        x('http://www.futhead.com/16/players/' + playerRating + '/test', 'body', [{
+            player: 'h1 a',
+            position: '.playercard-position',
+            rating: '.playercard-rating',
+            nation: '.playercard-nation img@src',
+            club: '.playercard-club img@src',
+            playerImage: '.playercard-picture img@src',
+            workrates: '.playercard-workrates',
+            stats: x('.card-large', {
+              pace: '.playercard-attr1',
+              shooting: '.playercard-attr2',
+              passing: '.playercard-attr3',
+              dribbling: '.playercard-attr4',
+              defending: '.playercard-attr5',
+              physical: '.playercard-attr6'
+                })
+           
+    }])(function(err, title) {
+      console.log("json file created for player with rank " + playerRating);
+    }).write('results.json')
+
+}
+
+
+var futClubs = function scrapeClubs(club){
+    var x = Xray();
+
+        x('http://www.futhead.com/16/clubs/' + club + '/', '.player-page-listing', [{
+            players: x('a',
+                {
+                    player: '.playercard-name',
+                    position: '.playercard-position',
+                    rating: '.playercard-rating',
+                    nation: '.playercard-nation img@src',
+                    club: '.playercard-club img@src',
+                    playerImage: '.playercard-picture img@src',
+                    workrates: '.playercard-workrates',
+                    stats: x('.card-small', {
+                      pace: '.playercard-attr1',
+                      shooting: '.playercard-attr2',
+                      passing: '.playercard-attr3',
+                      dribbling: '.playercard-attr4',
+                      defending: '.playercard-attr5',
+                      physical: '.playercard-attr6'
+                        })
+                    })
+           
+           
+           
+    }])(function(err, title) {
+      console.log("json file '" + club + "' created");
+    }).write(club + '.json')
+
+}
+exports.scrapePlayers = futPlayer;
+exports.scrapeClubs = futClubs;
 
